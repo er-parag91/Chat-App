3d3a83e6ed22d6d7037d2e5369985a2fdcb31750
ppatel81 <ppatel81@dxc.com>
2019-07-09 17:44:45 -0500

    now sharing user location

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,4 +8,18 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault();
     const message = e.target.elements.message.value;
     socket.emit('sendMessage', message);
+});
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Sorry! your browser does not support geo location at the moment!')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        const location = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }
+        socket.emit('sendLocation', location);
+    })
 })
--- src/index.js
+++ src/index.js
@@ -21,9 +21,19 @@ const PORT = process.env.PORT || 3000;
 io.on('connection', (socket) => {
     console.log('new websocket connection');
     socket.emit('message', 'Welcome!!');
+
     socket.broadcast.emit('message', 'A new has joined');
+
     socket.on('sendMessage', (message) => {
         io.emit('message', message)
+    });
+
+    socket.on('sendLocation', (location) => {
+        io.emit('message', `https://google.com/maps?q=${location.latitude},${location.longitude}`);
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!!');
     })
 });
 
