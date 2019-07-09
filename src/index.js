dffec66ff089a33a08a9841d5cc5d56947cc7d46
ppatel81 <ppatel81@dxc.com>
2019-07-09 13:25:01 -0500

    added initial setup for express server

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
+
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+});
