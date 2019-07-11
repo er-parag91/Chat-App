f8a6a54a7725dfd6ae1d40b0a868844467ceb2c8
ppatel81 <ppatel81@dxc.com>
2019-07-10 20:39:48 -0500

    sending messages to specific room user joined

--- public/js/chat.js
+++ public/js/chat.js
@@ -19,6 +19,7 @@ socket.on('message', (message) => {
     console.log(message);
 
     const html = Mustache.render($messageTemplate, {
+        username: message.username,
         createdAt: moment(message.createdAt).format('h:mm a'),
         message: message.text
     });
@@ -29,6 +30,7 @@ socket.on('locationMessage', (message) => {
     console.log(message);
 
     const html = Mustache.render($locationMessageTemplate, {
+        username: message.username,
         url: message.url,
         createdAt: moment(message.createdAt).format('h:mm a')
     });
--- src/index.js
+++ src/index.js
@@ -36,17 +36,20 @@ io.on('connection', (socket) => {
     })
 
     socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
         const filter = new Filter();
 
         if (filter.isProfane(message)) {
             return callback('Profanity is not allowed!');
         }
-        io.emit('message', generateMessage(message));
+        io.to(user.room).emit('message', generateMessage(user.username, message));
         callback();
     });
 
     socket.on('sendLocation', (location, callback) => {
-        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${location.latitude},${location.longitude}`));
+        const user = getUser(socket.id);
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${location.latitude},${location.longitude}`));
         callback();
     })
 
